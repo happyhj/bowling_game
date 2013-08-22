9adb2f90db6918bb7f4bdb61d79db50f7e5ea4fa
happyhj <happyhj@gmail.com>
2013-08-22 22:16:31 +0900

    Roll 클래스의 isRolled 메소드 작성

--- src/bowling/Roll.java
+++ src/bowling/Roll.java
@@ -3,12 +3,19 @@ package bowling;
 public class Roll {
 
 	private int score;
+	private boolean isRolled = false;
+	
 	public void setScore(int i) {
 		// TODO Auto-generated method stub
 		score = i;
+		isRolled = true;
 	}
 	public int getScore(int i) {
 		// TODO Auto-generated method stub
 		return score;
+	}
+	public boolean isRolled() {
+		// TODO Auto-generated method stub
+		return isRolled;
 	}
 }
--- test/bowling/RollTest.java
+++ test/bowling/RollTest.java
@@ -12,4 +12,16 @@ public class RollTest extends TestCase {
 		roll2.setScore(3);
 		assertEquals(3,	roll2.getScore(3));
 	}
+	public void testIsRolled() throws Exception {
+		Roll roll1 = new Roll();
+		Roll roll2 = new Roll();
+		assertEquals(false,	roll2.isRolled());
+		assertEquals(false,	roll1.isRolled());
+		roll1.setScore(0);
+		assertEquals(false,	roll2.isRolled());
+		assertEquals(true,	roll1.isRolled());
+		roll2.setScore(3);
+		assertEquals(true,	roll2.isRolled());
+		assertEquals(true,	roll1.isRolled());	
+	}
 }
